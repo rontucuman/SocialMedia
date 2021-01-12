d7e44ecf34d3fa972baa5c748fa3d5680072dad9
Ron <rontucuman@gmail.com>
2021-01-12 12:39:05 -0400

    Adding Post Controller and end point to get dummy post list from dummy repository

---
+++ SocialMedia/SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SocialMedia.Core.Entities;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+  public class PostRepository
+  {
+    public IEnumerable<Post> GetPosts()
+    {
+      var posts = Enumerable.Range(1, 10).Select(x => new Post
+      {
+        PostId = x,
+        UserId = x,
+        Date = DateTime.Now,
+        Description = $"Description Post {x}",
+        Image = $"Image Post {x} URL"
+      });
+
+      return posts;
+    }
+  }
+}
